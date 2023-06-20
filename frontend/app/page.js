b5e50c9aa4bddb1823c94d2db6ab7d0755626f27
nikhilpn725 <nikhilpn725@gmail.com>
2023-06-20 17:28:25 +0530

    next.js created in frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className='text-8xl text-center'>BookSwap Testing</h1>
+    </main>
+  )
+}
