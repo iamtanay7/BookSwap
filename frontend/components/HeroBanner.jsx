2d50b832d309bfecfce7dc99960429e8a31d1bbb
nikhilpn725 <nikhilpn725@gmail.com>
2023-07-04 22:57:05 +0530

    Hifi hero banner updated and styled

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -22,16 +22,32 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 import { BiArrowBack } from "react-icons/bi";
 import Link from "next/link";
+import Wrapper from "./Wrapper";
 
 const HeroBanner = () => {
   return (
-    <div className="relative text-white text-[20px] w-full max-w-[1360px] mx-auto ">
-      <div>
-        <img
-          src="assets/hero.svg"
-          className="aspect-[16/10] md:aspect-auto object-cover"
-        />
-      </div>
+    <div className="bg-black  w-full max-w-[1360px] mx-auto  flex-row flex mt-8">
+      <section className="bg-[#F6F6F6] flex items-center justify-center w-1/2 h-[350px] flex-col gap-8">
+        <div className="">
+          <div className="text-[#228D5A] text-[40px] font-semibold mb-4">
+            Beach Must Reads
+          </div>
+          <div className="text-green-600 text-[20px] font-medium">
+            Find your ideal summer read
+          </div>
+        </div>
+        <div>
+          <button className="w-[184px] h-[66px] relative hover:scale-105 ease-in-out duration-300">
+            <div className="w-[184px] h-[66px]  bg-[#228D5A] rounded-xl shadow" />
+            <div className="left-[31px] top-[18px] absolute text-[#FFF1F1] text-[24px] font-semibold">
+              Swap Now
+            </div>
+          </button>
+        </div>
+      </section>
+      <section className="bg-[#F5B961] w-1/2 h-[350px] flex justify-center items-center flex-shrink">
+        <img src="/assets/hero-books.svg" alt="books image" />
+      </section>
     </div>
   );
 };
--- frontend/components/NavIcons.jsx
+++ frontend/components/NavIcons.jsx
@@ -1,27 +1,13 @@
 import React from "react";
-
 import Link from "next/link";
-
-import { BsCart } from "react-icons/bs";
-import { IoMdHeartEmpty } from "react-icons/io";
-import { BiMenuAltRight } from "react-icons/bi";
-import { VscChromeClose } from "react-icons/vsc";
-import { AiOutlineSearch } from "react-icons/ai";
-import { BsChatDots } from "react-icons/bs";
-import { RxAvatar } from "react-icons/rx";
-
 const NavIcons = () => {
   return (
     <div className="-ml-14 justify-between gap-4 hidden md:flex">
       <Link href="">
-        {/* <section className="w-8 hidden  md:flex md:w-12  h-8 md:h-12 rounded   justify-center items-center hover:bg-black/[0.05] cursor-pointer relative transition-transform active:scale-90"> */}
         <div className="text-zinc-600  text-[16px] font-semibold">Browse</div>
-        {/* </section> */}
       </Link>
       <Link href="/wishlist">
-        {/* <section className="w-8 hidden  md:flex md:w-12 h-8 md:h-12 rounded-full  justify-center items-center hover:bg-black/[0.05] cursor-pointer relative transition-transform active:scale-90"> */}
         <div className="text-zinc-600  text-[16px] font-semibold">Wishlist</div>
-        {/* </section> */}
       </Link>
 
       <Link href="/chat">
