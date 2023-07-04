512bb2f1f5d90ba4b75387eef54a8f91a8751b97
nikhilpn725 <nikhilpn725@gmail.com>
2023-07-05 00:37:43 +0530

    Footer completed with hifi design/ homeScreen hifi coded

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -6,12 +6,13 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className=" text-white">
-      <section className="justify-center items-center flex p-4">
-        <div className="text-[10px] md:text-[14px] font-medium text-gray-600 hover:text-black cursor-pointer text-center md:text-left">
+    <footer className="bg-[#EBBA0C] h-[150px] w-full text-white">
+      <div className="flex items-center h-full justify-start pl-16  w-full">
+        <img src="/assets/bookswapw.png" className="" alt="bookswap logo" />
+        <div className="text-[10px] ml-auto pr-20 md:text-[14px] font-medium text-zinc-600 hover:text-black cursor-pointer text-center md:text-left">
           © 2023 BookSwap-Colab20, Inc. All Rights Reserved
         </div>
-      </section>
+      </div>
     </footer>
   );
 };
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,11 +8,24 @@ export default function Home() {
       <HeroBanner />
       <section className="w-full max-w-[1280px] px-5 md:px-20">
         <div className=" justify-start flex max-w-[800px]  my-[20px] md:my-[40px]">
-          {/* <div className="text-lg md:text-2xl font-light ">
-            Popular Books Near You
-          </div> */}
           <div class="w-[245px] h-[31px] text-zinc-600 text-[20px] font-medium">
             Popular Books Near You
+          </div>
+        </div>
+      </section>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5  gap-5 my-12 px-8  md:px-20">
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+      </div>
+      <section className="w-full max-w-[1280px] px-5 md:px-20">
+        <div className=" justify-start flex max-w-[800px]  my-[20px] md:my-[40px]">
+          <div class="w-[245px] h-[31px] text-zinc-600 text-[20px] font-medium">
+            Top Rated Books
           </div>
         </div>
       </section>
