e7796596d4f82aca8d56cdcf889b0dae73722a22
nikhilpn725 <nikhilpn725@gmail.com>
2023-07-05 00:42:05 +0530

    mobile responsive added for footer comp

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#EBBA0C] h-[150px] w-full text-white">
       <div className="flex items-center h-full justify-start pl-16  w-full">
-        <img src="/assets/bookswapw.png" className="" alt="bookswap logo" />
+        <img src="/assets/bookswapw.png" className="hidden md:block" alt="bookswap logo" />
         <div className="text-[10px] ml-auto pr-20 md:text-[14px] font-medium text-zinc-600 hover:text-black cursor-pointer text-center md:text-left">
           © 2023 BookSwap-Colab20, Inc. All Rights Reserved
         </div>
